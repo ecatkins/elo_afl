c62047f2fa2d8efc10f6fbb608bef9bbd4b28417
ecatkins <ecatkins@gmail.com>
2016-02-05 22:06:30 +0000

    Begin method documentation

--- elo_afl.py
+++ elo_afl.py
@@ -8,11 +8,11 @@ from sklearn.metrics import log_loss
 class Elo:
 	
 	def __init__(self):
-		self.current_ratings = {"Adelaide":1500,"Brisbane":1500,"Carlton":1500,"Collingwood":1500,"Essendon":1500,"Fremantle":1500,"Geelong":1500,"Gold Coast":1500,"GWS Giants":1500,"Hawthorn":1500,"Melbourne":1500,"North Melbourne":1500,"Port Adelaide":1500,"Richmond":1500, "St Kilda":1500,"Sydney":1500,"West Coast":1500,"Western Bulldogs":1500}
-		self.historical_ratings = {"Adelaide":{"Date":[],"Elo":[]},"Brisbane":{"Date":[],"Elo":[]},"Carlton":{"Date":[],"Elo":[]},"Collingwood":{"Date":[],"Elo":[]},"Essendon":{"Date":[],"Elo":[]},"Fremantle":{"Date":[],"Elo":[]},"Geelong":{"Date":[],"Elo":[]},"Gold Coast":{"Date":[],"Elo":[]},"GWS Giants":{"Date":[],"Elo":[]},"Hawthorn":{"Date":[],"Elo":[]},"Melbourne":{"Date":[],"Elo":[]},"North Melbourne":{"Date":[],"Elo":[]},"Port Adelaide":{"Date":[],"Elo":[]},"Richmond":{"Date":[],"Elo":[]}, "St Kilda":{"Date":[],"Elo":[]},"Sydney":{"Date":[],"Elo":[]},"West Coast":{"Date":[],"Elo":[]},"Western Bulldogs":{"Date":[],"Elo":[]}}
-		self.original_df = None
+		pass
 
 	def add_data(self, file):
+		'''File sourced from 
+		http://www.aussportsbetting.com/data/historical-afl-results-and-odds-data/'''
 		initial_data = pd.read_excel(file)
 		self.original_df = self.add_columns(initial_data)
 		# print(self.original_df)
@@ -21,7 +21,6 @@ class Elo:
 	def add_columns(self, initial_data):
 		initial_data = initial_data[0:1300]
 		initial_data = initial_data.sort(['Date'])
-
 		initial_data['home_odds_estimate'] = 0
 		initial_data['away_odds_estimate'] = 0
 		initial_data['home_prob'] = 0
@@ -34,32 +33,55 @@ class Elo:
 		self.current_ratings = {"Adelaide":1500,"Brisbane":1500,"Carlton":1500,"Collingwood":1500,"Essendon":1500,"Fremantle":1500,"Geelong":1500,"Gold Coast":1500,"GWS Giants":1500,"Hawthorn":1500,"Melbourne":1500,"North Melbourne":1500,"Port Adelaide":1500,"Richmond":1500, "St Kilda":1500,"Sydney":1500,"West Coast":1500,"Western Bulldogs":1500}
 		self.historical_ratings = {"Adelaide":{"Date":[],"Elo":[]},"Brisbane":{"Date":[],"Elo":[]},"Carlton":{"Date":[],"Elo":[]},"Collingwood":{"Date":[],"Elo":[]},"Essendon":{"Date":[],"Elo":[]},"Fremantle":{"Date":[],"Elo":[]},"Geelong":{"Date":[],"Elo":[]},"Gold Coast":{"Date":[],"Elo":[]},"GWS Giants":{"Date":[],"Elo":[]},"Hawthorn":{"Date":[],"Elo":[]},"Melbourne":{"Date":[],"Elo":[]},"North Melbourne":{"Date":[],"Elo":[]},"Port Adelaide":{"Date":[],"Elo":[]},"Richmond":{"Date":[],"Elo":[]}, "St Kilda":{"Date":[],"Elo":[]},"Sydney":{"Date":[],"Elo":[]},"West Coast":{"Date":[],"Elo":[]},"Western Bulldogs":{"Date":[],"Elo":[]}}
 
+	def initial_ranking(self, row, home_team, away_team, mean_reversion):
+		''' Calculates the current rankings of the teams including update
+			for preseason mean reversion if required
+		Args:
+			row: current row from match dataframe
+			home_team: home team name
+			away_team: away team name
+			mean_reversion: mean reversion factor for each team after the season ends
+		Returns:
+			(home_ranking, away_ranking) -> Current home and away team rankings
+		'''
+
+		if row['Round']  == 1:
+			home_ranking = 1500 + (self.current_ratings[home_team] - 1500) * mean_reversion
+			self.current_ratings[home_team] = home_ranking
+			away_ranking = 1500 + (self.current_ratings[away_team] - 1500) * mean_reversion
+			self.current_ratings[away_team] = away_ranking
+		else:
+			home_ranking = self.current_ratings[home_team]
+			away_ranking = self.current_ratings[away_team]
+		return home_ranking, away_ranking
 
 
-	def run_model(self, k,home_field,mean_reversion,margin_beta):
+	def run_model(self, k, home_field,mean_reversion,margin_smoothing):
+		'''  Calculates historical and current elo ratings
+		Args: 
+			k: the main levearge point to customise the algorithm for different domains
+			home_field: the point advantage given to the home team
+			mean_reversion: mean reversion factor for each team after the season ends
+			margin_smoothing: smoothing parameter for the margin multiplier
+
+		Returns: 
+			The updated data frame with elo values and implied odds
+
+		'''
 		self.reset_ratings()
-		count = 0
 		df = self.original_df.copy()
 		
 		for row_index, row in df.iterrows():
+			
+			# General information
 			home_team = row['Home Team']
 			away_team = row['Away Team']
-
-
-		
-			# Preseason reversion to the mean
-			if row['Round'] == 1:
-				new_home_ranking = 1500 + (self.current_ratings[home_team] - 1500) * mean_reversion
-				self.current_ratings[home_team] = new_home_ranking
-				new_away_ranking = 1500 + (self.current_ratings[away_team] - 1500) * mean_reversion
-				self.current_ratings[away_team] = new_away_ranking
-		
-			# General
 			home_score = row['Home Score']
 			away_score = row['Away Score']
-			home_ranking = self.current_ratings[home_team]
-			away_ranking = self.current_ratings[away_team]
-
+			home_ranking, away_ranking = self.initial_ranking(row, home_team, away_team, mean_reversion)
+		
+			
+			
 			# Estimate odds for games
 			# Pr(A) = 1 / (10^(-ELODIFF/400) + 1)
 			prob_home = 1 / (10**((away_ranking-home_ranking)/400)+1)
@@ -98,7 +120,7 @@ class Elo:
 				actual_result = 0.5
 			else:
 				actual_result = 0
-			new_rank = self.elo_calculation(k,point_differential,old_rank,opponent_rank, winners_rank,losers_rank,actual_result, home_field_advantage,margin_beta)
+			new_rank = self.elo_calculation(k,point_differential,old_rank,opponent_rank, winners_rank,losers_rank,actual_result, home_field_advantage,margin_smoothing)
 			self.current_ratings[home_team] = new_rank
 			self.historical_ratings[home_team]['Date'].append(row['Date'])
 			self.historical_ratings[home_team]['Elo'].append(new_rank)
@@ -113,7 +135,7 @@ class Elo:
 				actual_result = 0.5
 			else:
 				actual_result = 0
-			new_rank = self.elo_calculation(k,point_differential,old_rank,opponent_rank, winners_rank,losers_rank,actual_result, home_field_advantage,margin_beta)
+			new_rank = self.elo_calculation(k,point_differential,old_rank,opponent_rank, winners_rank,losers_rank,actual_result, home_field_advantage,margin_smoothing)
 			self.current_ratings[away_team] = new_rank
 			self.historical_ratings[away_team]['Date'].append(row['Date'])
 			self.historical_ratings[away_team]['Elo'].append(new_rank)
@@ -122,7 +144,7 @@ class Elo:
 
 		return self.elo_df
 
-	def elo_calculation(self, k, point_differential,old_rank,opponent_rank,winners_rank,losers_rank,actual_result,home_field_advantage,margin_beta):
+	def elo_calculation(self, k, point_differential,old_rank,opponent_rank,winners_rank,losers_rank,actual_result,home_field_advantage,margin_smoothing):
 		if winners_rank == old_rank and home_field_advantage > 0:
 			winners_advantage = home_field_advantage
 		elif winners_rank == opponent_rank and home_field_advantage < 0:
@@ -133,15 +155,15 @@ class Elo:
 			winners_advantage = home_field_advantage
 		
 
-		margin_multiplier = np.log(abs(point_differential)+1) * margin_beta / (0.001*(winners_rank + winners_advantage - losers_rank) + margin_beta)
+		margin_multiplier = np.log(abs(point_differential)+1) * margin_smoothing / (0.001*(winners_rank + winners_advantage - losers_rank) + margin_smoothing)
 		expected_result = 1 / (1 + 10**((opponent_rank-(old_rank+home_field_advantage))/400))
 		new_rank = old_rank + k * margin_multiplier * (actual_result - expected_result)
 		return new_rank
 
-	def run_show(self,k,home_field,mean_reversion,margin_beta,teams=False):
+	def run_plot(self,k,home_field,mean_reversion,margin_smoothing,teams=False):
 		import matplotlib.pyplot as plt
 
-		elo_df = self.run_model(k,home_field,mean_reversion,margin_beta)
+		elo_df = self.run_model(k,home_field,mean_reversion,margin_smoothing)
 		elo_ratings = self.current_ratings
 		elo_historical = self.historical_ratings
 
@@ -161,7 +183,7 @@ class Elo:
 
 		plt.show()
 
-	def test_parameters(self, k_range, home_field_range, mean_reversion_range, margin_beta_range):
+	def test_parameters(self, k_range, home_field_range, mean_reversion_range, margin_smoothing_range):
 		
 		best = None
 		best_score = 10000000
@@ -169,31 +191,31 @@ class Elo:
 		for k in k_range:
 			for home_field in home_field_range:
 				for mean_reversion in mean_reversion_range:
-					for margin_beta in margin_beta_range:
-						elo_df = self.run_model(k, home_field, mean_reversion, margin_beta)
+					for margin_smoothing in margin_smoothing_range:
+						elo_df = self.run_model(k, home_field, mean_reversion, margin_smoothing)
 						not_draw = elo_df['home_result'] != 0.5
 						actual = list(elo_df['home_result'][not_draw])
 						predicted = list(elo_df['home_prob'][not_draw])
 						error = log_loss(actual, predicted)
 						if error < best_score:
 							best_score = error
-							best = k, home_field, mean_reversion, margin_beta
-						print(k, home_field, mean_reversion, margin_beta)
+							best = k, home_field, mean_reversion, margin_smoothing
+						print(k, home_field, mean_reversion, margin_smoothing)
 						print(error)
 		print("BEST HYPERPARAMETER SET")
 		print(best)
 		print(best_score)
 
 
-# my_elo = Elo()
+my_elo = Elo()
 
-# my_elo.add_data('afl.xlsx')
+my_elo.add_data('afl.xlsx')
 
-# my_elo.run_show(16,110,0.70,12.2,teams=['Fremantle','Melbourne','West Coast'])
+my_elo.run_plot(16,110,0.70,12.2,teams=['Fremantle','Melbourne','West Coast'])
 
-# my_elo.run_show(16,200,0.6,20)
+# my_elo.run_plot(16,200,0.6,20)
 
-# my_elo.test_parameters(k_range = [14,16,18], home_field_range = [180,200,220],mean_reversion_range=[0,56, 0.58],margin_beta_range = [19,20,21,22])
+# my_elo.test_parameters(k_range = [14,16,18], home_field_range = [180,200,220],mean_reversion_range=[0,56, 0.58],margin_smoothing_range = [19,20,21,22])
 
 
 
